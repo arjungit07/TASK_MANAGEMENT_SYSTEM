580e5e5a6d3f640114a7f1d63224b32e3115c374
Harsh Ganjsinghani <aganjsinghani@gmail.com>
2024-09-16 20:05:20 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(
 );
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin",process.env.FRONTEND_URL);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
